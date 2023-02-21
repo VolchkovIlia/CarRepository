7c3bd3f1bff9709bd80cfe8f7f94a9631c9824e7
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-02-21 17:57:29 +0300

    Add files via upload

---
+++ CarRepository/CarRepository/CarRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRepositoryMDK
+{
+    public class CarRepository
+    {
+        private List<Car> cars = new List<Car>();
+
+        public List<Car> GetAll()
+        {
+            return cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return cars.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Insert(Car car)
+        {
+            cars.Add(car);
+        }
+
+        public void Delete(int id)
+        {
+            Car car = GetById(id);
+            if (car != null)
+            {
+                cars.Remove(car);
+            }
+        }
+
+        public void Update(int id, Car car)
+        {
+            Car existingCar = GetById(id);
+            if (existingCar != null)
+            {
+                existingCar.Make = car.Make;
+                existingCar.Model = car.Model;
+                existingCar.Year = car.Year;
+                existingCar.Color = car.Color;
+            }
+        }
+
+        public List<Car> Find(Predicate<Car> predicate)
+        {
+            return cars.FindAll(predicate);
+        }
+    }
+}
---
+++ CarRepository/CarRepository/Program.cs
@@ -0,0 +1,38 @@
+﻿using CarRepositoryMDK;
+
+
+CarRepository repository = new CarRepository();
+
+// Добавление автомобилей
+repository.Insert(new Car { Id = 1, Make = "Toyota", Model = "Camry", Year = 2018, Color = "Silver" });
+repository.Insert(new Car { Id = 2, Make = "Honda", Model = "Accord", Year = 2017, Color = "White" });
+repository.Insert(new Car { Id = 3, Make = "Nissan", Model = "Altima", Year = 2016, Color = "Black" });
+
+// Получение всех автомобилей
+List<Car> allCars = repository.GetAll();
+Console.WriteLine("Все машины:");
+foreach (Car car in allCars)
+{
+  Console.WriteLine($"Id: {car.Id}, Make: {car.Make}, Model: {car.Model}, Year: {car.Year}, Color: {car.Color}");
+}
+
+// Получение автомобиля по ID
+Car carById = repository.GetById(2);
+Console.WriteLine("Автомобиль под  Id 2:");
+Console.WriteLine($"Make: {carById.Make}, Model: {carById.Model}, Year: {carById.Year}, Color: {carById.Color}");
+
+// Обновление автомобиля
+Car updatedCar = new Car { Make = "Honda", Model = "Civic", Year = 2019, Color = "Red" };
+repository.Update(2, updatedCar);
+Car carAfterUpdate = repository.GetById(2);
+Console.WriteLine("Обновить автомобиль по Id 2:");
+Console.WriteLine($"Make: {carAfterUpdate.Make}, Model: {carAfterUpdate.Model}, Year: {carAfterUpdate.Year}, Color: {carAfterUpdate.Color}");
+
+// Удаление автомобиля
+Console.WriteLine("Удвляем автомобиль под Id 1.....");
+repository.Delete(1);
+Console.WriteLine("Спсиок авто после удаления Id 1:");
+foreach (Car car in allCars)
+{
+  Console.WriteLine($"Id: {car.Id}, Make: {car.Make}, Model: {car.Model}, Year: {car.Year}, Color: {car.Color}");
+}
